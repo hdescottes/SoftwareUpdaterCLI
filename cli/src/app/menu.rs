bc1d47e2651a609e61b5e4ee9a2c954d6f5b7095
HugoD <hudescottes@hotmail.fr>
2025-12-25 16:36:44 +0100

    chore: Add integration tests

--- cli/src/app/menu.rs
+++ cli/src/app/menu.rs
@@ -3,6 +3,7 @@ use crate::app::user_input::{DialoguerInput, UserInput};
 use crate::domain::custom_app::CustomApp;
 use crate::storage::store::load_apps;
 use crate::use_cases::custom_app::{add_custom, list_custom, remove_custom};
+use crate::use_cases::remove_select::{DialoguerRemoveSelect, RemoveSelect};
 use std::path::Path;
 
 const FILE_PATH: &str = "cli/apps.json";
@@ -10,21 +11,25 @@ const FILE_PATH: &str = "cli/apps.json";
 pub fn custom_menu() {
     let input = DialoguerInput;
     let menu = DialoguerMenuSelect;
+    let remove = DialoguerRemoveSelect;
 
     custom_menu_with(
         &input,
         &menu,
+        &remove,
         FILE_PATH.as_ref(),
     );
 }
 
-pub fn custom_menu_with<I, M>(
+pub fn custom_menu_with<I, M, R>(
     input: &I,
     menu: &M,
+    remove: &R,
     file_path: &Path,
 ) where
     I: UserInput,
     M: MenuSelect,
+    R: RemoveSelect
 {
     let mut apps = load_apps(file_path);
 
@@ -38,16 +43,17 @@ pub fn custom_menu_with<I, M>(
 
         let selection = menu.select(&options);
 
-        if !handle_menu_selection(selection, &mut apps, input, file_path) {
+        if !handle_menu_selection(selection, &mut apps, input, remove, file_path) {
             break;
         }
     }
 }
 
-pub fn handle_menu_selection<I: UserInput>(
+pub fn handle_menu_selection<I: UserInput, S: RemoveSelect>(
     selection: usize,
     apps: &mut Vec<CustomApp>,
     input: &I,
+    remove: &S,
     file_path: &Path,
 ) -> bool {
     match selection {
@@ -60,7 +66,7 @@ pub fn handle_menu_selection<I: UserInput>(
             true
         }
         2 => {
-            remove_custom(apps, file_path);
+            remove_custom(apps, remove, file_path);
             true
         }
         3 => false,
--- cli/tests/mocks/mod.rs
+++ cli/tests/mocks/mod.rs
@@ -1,2 +1,3 @@
 pub mod mock_inputs;
 pub mod mock_menu;
+pub mod mock_remove;

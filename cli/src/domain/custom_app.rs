3da3d31dacb06dc87885d77756a92e72d37c895a
HugoD <hudescottes@hotmail.fr>
2025-12-27 00:24:04 +0100

    chore: api to retrieve last version

--- cli/src/domain/custom_app.rs
+++ cli/src/domain/custom_app.rs
@@ -5,5 +5,5 @@ pub struct CustomApp {
     pub name: String,
     pub update_command: String,
     pub current_version_path: String,
-    pub latest_version_command: String,
+    pub latest_version_url: String,
 }
